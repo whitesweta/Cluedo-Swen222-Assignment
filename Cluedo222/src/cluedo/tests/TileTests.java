47dfafcc6bcc6e82ff0695c2fe1814da192183c1
libunamari <marialibunao@gmail.com>
2014-08-16 15:40:03 +1200

    Fixed bug in SelectCharFrame that produces nullpointers when 6 players are created.

--- Cluedo222/src/cluedo/tests/TileTests.java
+++ Cluedo222/src/cluedo/tests/TileTests.java
@@ -1,5 +1,22 @@
 package cluedo.tests;
 
-public class TileTests {
+import static org.junit.Assert.*;
+import cluedo.other.Position;
+import cluedo.tile.*;
 
+import org.junit.*;
+
+public class TileTests {
+	@Test
+	public void secretTileTest1(){
+		SecretTile st = new SecretTile(new Position(0,0),null,null);
+		try{
+			st.movePlayerOut();
+			fail("Should not be able to move a player out of tile");
+		}
+		catch(RuntimeException e){	
+		}
+	}
+	
+	
 }
